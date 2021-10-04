ff0f3839e9f6a49e674cb30f8a03ab9cfca0b44d
Laytonmar05 <marshalllt@g.crookcountyschools.org>
2021-10-04 08:25:28 -0700

    Lab Complete

--- classes.js
+++ classes.js
@@ -1,15 +1,20 @@
 class Animal {
-    constructor(height, width) {
+    constructor(name, species, age) {
       this.name = name;
       this.species = species;
       this.age = age;
     }
-}
 
-function nameAndSpecies() {
-    let ani = `${this.name} is a ${this.species}`;
+
+ nameAndSpecies() {
+    let ani = console.log(`${this.name} is a ${this.species} and he is ${this.age} years old`);
     return ani;
   }
-let monkey = new Animal ("Maurice", "monkey")
+}
+let monkey = new Animal ("Maurice", "monkey", "3")
+let dog = new Animal ("Jerry", "dog","7" )
+let goldfish = new Animal ("Bert", "goldfish", "1")
 
-console.log (monkey.nameAndSpecies())
+monkey.nameAndSpecies();
+dog.nameAndSpecies();
+goldfish.nameAndSpecies();
